3a18f4cd54fb47e86948091769bd76740e0f48ba
lubcountcooper <lub@countcooper.nl>
2019-03-28 10:14:22 +0800

    added option to add entries and topics, navbar, and styling of pages

--- logs/urls.py
+++ logs/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 
 from logs import views
 
+app_name = 'logs'
+
 urlpatterns = [
-    path('', views.topic_overview),
-    path('topic/<topic>', views.topic),
+    path('', views.topic_overview, name="index"),
+    path('new_topic', views.new_topic, name="new_topic"),
+    path('topic/<topic>', views.topic, name="topic"),
 ]
+
--- logs/views.py
+++ logs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from logs.models import Topic, Entry
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from .forms import TopicForm, EntryForm
+
 # Create your views here.
 
 
@@ -7,27 +12,67 @@ def topic_overview(requests):
     context = {}
 
     my_dict = {}
+    for topic in Topic.objects.all():
+        my_dict[topic] = []
+
     for entry in Entry.objects.all():
-        if entry.topic not in my_dict:
-            my_dict[entry.topic] = [entry.__str__()]
-        else:
-            my_dict[entry.topic].append(entry.__str__())
+        my_dict[entry.topic].append(entry.__str__())
 
     context["input"] = my_dict
 
-    return render(requests, "topic_overview.html", context)
+    return render(requests, "topics_overview.html", context)
 
 
-def topic(requests, topic):
+def topic(request, topic):
+
     context = {}
-    context["topic"] = topic
-    context["entries"] = []
+    entries = []
 
     for entry in Entry.objects.all():
         if entry.topic.text.lower() == topic.lower():
-            context["entries"].append(entry.text)
+            entries.append(entry.text)
 
-    if context["entries"] == []:
-        context["entries"].append("No entries for this class")
+    if entries is False:
+        entries.append("No entries for this class")
 
-    return render(requests, "topic.html", context)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = Topic.objects.get(text=topic)
+            new_entry.save()
+            return HttpResponseRedirect(reverse("logs:topic", args=[topic]))
+
+    context["form"] = form
+    context["URL"] = request.path
+    context["entries"] = entries
+    context["topic"] = topic
+
+    return render(request, "topic_specific_overview.html", context)
+
+
+def new_topic(request):
+    message = "Enter your new topic here."
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+
+        # collect all topic to later check if the new entered topic does not already exist.
+        topic_list = []
+        for topic in Topic.objects.all():
+            topic_list.append(topic.text.lower())
+
+        # check if form is valid and if topic does not already exists, make the new topic.
+        if form.is_valid() and form.data["text"].lower() not in topic_list:
+            form.save()
+            return HttpResponseRedirect(reverse("logs:index"))
+        # if it does exist, inform the user.
+        elif form.is_valid():
+            message = "The entered topic already exists. Enter your new topic here."
+
+    context = {'form': form, "message": message}
+
+    return render(request, 'new_topic.html', context)
