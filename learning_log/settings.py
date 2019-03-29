7062ce3991c52b70bd1d64f10a6376cf50953894
lubcountcooper <lub@countcooper.nl>
2019-03-29 16:20:51 +0800

    testing database

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learning_log',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['SECRET_KEY_POSTGRES'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
